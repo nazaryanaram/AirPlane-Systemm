385a1b36e74308f7a1f352670afd9dfd06fc64d6
nazaryanaram <aramnazaryan580@gmail.com>
2024-07-05 11:34:36 +0400

    header

---
+++ include/header.h
@@ -0,0 +1,37 @@
+#ifndef FLIGHT_BOOKING_H
+#define FLIGHT_BOOKING_H
+
+#include <stdio.h>
+
+typedef struct
+{
+    int ID;
+    int age;
+    char *name;
+    char *PhoneNumber;
+    char *email;
+} User;
+
+typedef struct
+{
+    int year;
+    int month;
+    int day;
+} Date;
+
+typedef struct
+{
+    int ID;
+    char *from;
+    char *to;
+    Date date;
+    char *model;
+} Flight;
+
+typedef struct
+{
+    int user_id;
+    int flight_id;
+} Book_Flights;
+
+#endif
